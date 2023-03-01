994077a661c278de137afd8b62f848040eec473b
Antoine ROBIN <antoine.robin@aticommuity.fr>
2023-03-01 11:46:59 +0100

    Initial commit

---
+++ GreenIT/GreenITPowerShell.cs
@@ -0,0 +1,41 @@
+﻿using System.Management.Automation;
+using System.Text;
+
+namespace GreenIT
+{
+    public static class GreenITPowerShell
+    {
+        private static PowerShell _console = PowerShell.Create();
+
+        public static string Command(string script)
+        {
+            string errorMsg = string.Empty;
+            string output = string.Empty;
+            PSDataCollection<PSObject> outputCollection = new();
+            StringBuilder stringBuilder = new();
+
+            _console.AddCommand("Set-ExecutionPolicy").AddParameter("ExecutionPolicy", "RemoteSigned").AddParameter("scope", "LocalMachine").Invoke();
+
+            _console.AddScript(script);
+            _console.AddCommand("Out-String");
+
+            _console.Streams.Error.DataAdded += (object sender, DataAddedEventArgs e) =>
+            {
+                errorMsg = ((PSDataCollection<ErrorRecord>)sender)[e.Index].ToString();
+            };
+
+            IAsyncResult result = _console.BeginInvoke<PSObject, PSObject>(null, outputCollection);
+            _console.EndInvoke(result);
+
+            foreach (var outputItem in outputCollection)
+            {
+                stringBuilder.AppendLine(outputItem.BaseObject.ToString());
+            }
+
+            _console.Commands.Clear();
+
+            if (!string.IsNullOrEmpty(errorMsg)) return errorMsg;
+            return stringBuilder.ToString().Trim();
+        }
+    }
+}
---
+++ GreenIT/GreenITService.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace GreenIT
+{
+    public class GreenITService
+    {
+        private readonly BackgroundWorker _worker;
+        private string _path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        public GreenITService()
+        {
+            _worker = new BackgroundWorker();
+            _worker.WorkerSupportsCancellation = true;
+            _worker.DoWork += Engine;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Stating Engine...");
+            _worker.RunWorkerAsync();
+        }
+
+        public void Engine(object? sender, DoWorkEventArgs e)
+        {
+            Console.WriteLine("Engine started");
+            while (true)
+            {
+                File.AppendAllText(@"C:\ProgramData\OCS Inventory NG\Agent\GreenIT.log", GreenITPowerShell.Command(_path + @"\helloworld.ps1"));
+                // Anti-CPUBreaker
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Engine stopped");
+            _worker.CancelAsync();
+        }
+    }
+}
